6c9d95c4ba741f4e826191999cfd69e7c9a38f36
Ruiner85 <paul.m.mackay85@gmail.com>
2020-02-08 20:31:21 +0000

    populate suppliers in seeds.rb

---
+++ db/seeds.rb
@@ -0,0 +1,26 @@
+require_relative("../models/supplier.rb")
+require_relative("../models/stock_item.rb")
+require_relative("../models/product.rb")
+require("pry-byebug")
+
+Product.delete_all()
+Supplier.delete_all()
+StockItem.delete_all()
+
+supplier1 = Supplier.new({
+  "name" => "Barter Books", "email" => "bb@barterbooks.co.uk", "contact_number" => "+44 (0)1665 604 888", "website" => "http://www.barterbooks.co.uk"
+  })
+supplier1.save()
+
+supplier2 = Supplier.new({
+  "name" => "Wordery", "email" => "help@wordery.com", "contact_number" => "N/A", "website" => "http://wordery.com"
+  })
+supplier2.save()
+
+supplier3 = Supplier.new({
+  "name" => "World of Books", "email" => "customerservice@worldofbooks.com", "contact_number" => "+44 (0)1903 507 544", "website" => "http://worldofbooks.com"
+  })
+supplier3.save()
+
+stock_item1 = StockItem.new({"quantity" => ""})
+stock_item1.save()
