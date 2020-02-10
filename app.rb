14ac9c699337de73aee0662f194bede6f76731f2
Ruiner85 <paul.m.mackay85@gmail.com>
2020-02-10 12:51:19 +0000

    fix indentation & populate layout.erb

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('controllers/products_controller')
 require_relative('controllers/suppliers_controller')
+require_relative('./models/product')
+require_relative('./models/supplier')
+also_reload('./models/*')
 
 get '/' do
   erb(:index)
