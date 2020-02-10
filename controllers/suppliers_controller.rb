cad91c18e44d95258ada28a482c83f9e91f69a81
Ruiner85 <paul.m.mackay85@gmail.com>
2020-02-10 11:25:59 +0000

    update suppliers_controller.rb

--- controllers/suppliers_controller.rb
+++ controllers/suppliers_controller.rb
@@ -8,7 +8,33 @@ get '/suppliers' do
   erb(:"suppliers/index")
 end
 
+get '/suppliers/new' do
+  erb(:"suppliers/new")
+end
+
 get '/suppliers/:id' do
-  @suppliers = Supplier.find(params['id'].to_i)
+  @supplier = Supplier.find(params['id'].to_i)
   erb(:"suppliers/show")
+end
+
+post '/suppliers' do
+  @supplier = Supplier.new(params)
+  @supplier.save()
+  erb(:"suppliers/create")
+end
+
+get '/suppliers/:id/edit' do
+  @supplier = Supplier.find(params[:id])
+  erb(:"suppliers/edit")
+end
+
+post '/suppliers/:id' do
+  Supplier.new(params).update
+  redirect to '/suppliers'
+end
+
+post '/suppliers/:id/delete' do
+  supplier = Supplier.find(params[:id])
+  supplier.delete()
+  redirect to '/suppliers'
 end
